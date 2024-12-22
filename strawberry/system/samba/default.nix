78f3d98e20bb7e0d2111b8f12f993b29c4f72306
QuaternionWithSoul <nailzagru@gmail.com>
2024-12-22 18:43:45 +0300

    samba

--- strawberry/system/samba/default.nix
+++ strawberry/system/samba/default.nix
@@ -1,1 +1,14 @@
-{ ... }: {}
+{ userName, ... }: {
+  services.samba = {
+    enable = true;
+    security = "user";
+    shares = {
+      "0N-Files" = {
+        path = "/home/${userName}/Files";
+        browseable = true;
+        writable = true;
+        validUsers = [ ${userName} ];
+      };
+    };
+  };
+}
