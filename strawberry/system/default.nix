30d70cd8f77bb16f0d512127af559baa8b70a64b
QuaternionWithSoul <nailzagru@gmail.com>
2025-01-01 18:38:14 +0300

    fix

--- strawberry/system/default.nix
+++ strawberry/system/default.nix
@@ -12,7 +12,6 @@
   boot = {
     extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
     kernelModules = [ "v4l2loopback" ];
-    kernelPackages = pkgs.linuxPackages_latest;
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
