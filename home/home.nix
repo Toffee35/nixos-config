139b58c88d8e4ef1912f254aa049145e91a54dec
Toffee35 <nailzagru@gmail.com>
2025-02-25 23:19:50 +0300

    gtk home theming

--- home/home.nix
+++ home/home.nix
@@ -1,1 +1,21 @@
-{ ... }: { }
+{ pkgs, ... }: {
+  gtk = {
+    enable = true;
+
+    cursorTheme = {
+      package = pkgs.quintom-cursor-theme;
+      name = "Quintom_Ink";
+      size = 24;
+    };
+
+    iconTheme = {
+      package = pkgs.colloid-icon-theme;
+      name = "Colloid-Dark";
+    };
+
+    theme = {
+      package = pkgs.fluent-gtk-theme;
+      name = "Fluent-Dark";
+    };
+  };
+}
