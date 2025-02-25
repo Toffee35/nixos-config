2fab8622e473ac947c4317ce0e5719e7a8e18e01
Toffee35 <nailzagru@gmail.com>
2025-02-25 22:14:50 +0300

    added home-manager

--- flake.nix
+++ flake.nix
@@ -1,6 +1,12 @@
 {
+  description = "NixOS configuration";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     prismlauncher = {
       url = "github:Diegiwg/PrismLauncher-Cracked";
       inputs.flake-compat.follows = "";
@@ -10,14 +16,29 @@
   outputs = { self, nixpkgs, prismlauncher, ... }:
     let
       system = "x86_64-linux";
+
       hostname = "0NDesktop";
       username = "n";
+
       flakedir = "~/nixos-config/";
+
+      specialArgs = { inherit hostname username flakedir prismlauncher; };
     in {
       nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit hostname username flakedir prismlauncher; };
-        modules = [ ./system.nix ];
+        inherit system specialArgs;
+        modules = [
+          ./system.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.extraSpecialArgs = specialArgs;
+
+            home-manager.users.ryan = import ./home.nix;
+          }
+        ];
       };
     };
 }
