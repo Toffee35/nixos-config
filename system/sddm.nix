8be20e2a9905fcde7c428563f6cec899a9360c02
Toffee35 <nailzagru@gmail.com>
2025-03-01 17:56:44 +0300

    fix

--- system/sddm.nix
+++ system/sddm.nix
@@ -10,6 +10,7 @@
                 owner = "Keyitdev";
                 repo = "sddm-astronaut-theme";
                 rev = "5e39e0841d4942757079779b4f0087f921288af6";
+                sha256 = "09vi9dr0n0bhq8cj4jq1h17jw2ssi79zi9lhn0j6kgbxrqk2g8vf";
               }
             }/* $out/share/sddm/themes/astronaut/
             sed -i 's/^ConfigFile.*/ConfigFile=Themes\/purple_leaves.conf/' $out/share/sddm/themes/astronaut/metadata.desktop
