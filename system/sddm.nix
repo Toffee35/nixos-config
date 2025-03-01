b3f60ea7a0b83f34655475cf41cbdf4f883b618f
Toffee35 <nailzagru@gmail.com>
2025-03-01 16:43:45 +0300

    ашч

--- system/sddm.nix
+++ system/sddm.nix
@@ -1,6 +1,24 @@
 { pkgs, ... }: {
+  nixpkgs.overlays = [
+    (self: super: {
+      kdePackages = super.kdePackages // {
+        sddm = super.kdePackages.sddm.overrideAttrs (oldAttrs: {
+          installPhase = ''
+            ${oldAttrs.installPhase}
+            mkdir -p $out/share/sddm/themes/my-astronaut
+            cp -R ${builtins.fetchGit {
+              url = "https://github.com/Keyitdev/sddm-astronaut-theme.git";
+            }}/* $out/share/sddm/themes/my-astronaut/
+            sed -i 's/^ConfigFile.*/ConfigFile=Themes\/purple_leaves.conf/' $out/share/sddm/themes/my-astronaut/metadata.desktop
+          '';
+        })
+      };
+    })
+  ];
+
   services.displayManager.sddm = {
     enable = true;
-    theme = "${pkgs.sddm-astronaut}/share/sddm/themes/sddm-astronaut-theme";
+    package = pkgs.plasma5Packages.sddm;
+    theme = "my-astronaut";
   };
 }
