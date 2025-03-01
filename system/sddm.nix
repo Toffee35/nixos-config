582080fb10b98b85c8f6a7ea3e257ec37271b091
Toffee35 <nailzagru@gmail.com>
2025-03-01 16:46:22 +0300

    fix

--- system/sddm.nix
+++ system/sddm.nix
@@ -6,12 +6,14 @@
           installPhase = ''
             ${oldAttrs.installPhase}
             mkdir -p $out/share/sddm/themes/my-astronaut
-            cp -R ${builtins.fetchGit {
-              url = "https://github.com/Keyitdev/sddm-astronaut-theme.git";
-            }}/* $out/share/sddm/themes/my-astronaut/
+            cp -R ${
+              builtins.fetchGit {
+                url = "https://github.com/Keyitdev/sddm-astronaut-theme.git";
+              }
+            }/* $out/share/sddm/themes/my-astronaut/
             sed -i 's/^ConfigFile.*/ConfigFile=Themes\/purple_leaves.conf/' $out/share/sddm/themes/my-astronaut/metadata.desktop
           '';
-        })
+        });
       };
     })
   ];
