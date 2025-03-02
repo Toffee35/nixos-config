f721dc4eb4da94f9d3b5a8b4acc00e1881cb568b
Toffee35 <nailzagru@gmail.com>
2025-03-02 22:30:01 +0300

    fix

--- home/qtile/config.py
+++ home/qtile/config.py
@@ -117,3 +117,16 @@ mouse = [
 ]
 
 follow_mouse_focus = True
+
+floating_layout = layout.Floating(
+    float_rules=[
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),
+        Match(wm_class="makebranch"),
+        Match(wm_class="maketag"),
+        Match(wm_class="ssh-askpass"),
+        Match(title="branchdialog"),
+        Match(title="pinentry"),
+        Match(title="Proton VPN"),
+    ]
+)
