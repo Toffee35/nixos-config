008a5edc84878419385bd7a48dfcbdc0d208c9e3
Toffee35 <nailzagru@gmail.com>
2025-03-02 15:30:29 +0300

    fix

--- system/virt-manager.nix
+++ system/virt-manager.nix
@@ -1,25 +1,8 @@
-{ pkgs, ... }: {
+{ ... }: {
   programs.virt-manager.enable = true;
 
   virtualisation = {
-    libvirtd = {
-      enable = true;
-
-      qemu = {
-        package = pkgs.qemu_kvm;
-        runAsRoot = true;
-        swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [
-            (pkgs.OVMF.override {
-              secureBoot = true;
-              tpmSupport = true;
-            }).fd
-          ];
-        };
-      };
-    };
+    libvirtd.enable = true;
 
     spiceUSBRedirection.enable = true;
   };
