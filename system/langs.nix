5a184d4e842368e87814bf6b1e5b9cf3b9c66144
Toffee35 <nailzagru@gmail.com>
2025-04-03 16:59:03 +0300

    fix

--- system/langs.nix
+++ system/langs.nix
@@ -30,7 +30,6 @@
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
     OPENSSL_DIR = "${pkgs.openssl.dev}";
     OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
-    LD_LIBRARY_PATH = "${pkgs.openssl.out}/lib";
     RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
   };
 }
