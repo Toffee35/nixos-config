29f980b5592e15a452d377775406881ad095dae6
Toffee35 <nailzagru@gmail.com>
2025-04-03 16:54:04 +0300

    fix

--- home/qtile/config.py
+++ home/qtile/config.py
@@ -3,24 +3,28 @@ from libqtile.config import Click, Drag, Group, Key, Screen, Match
 from libqtile.lazy import lazy
 import os, logging
 
-home = os.path.expanduser('~')
+home = os.path.expanduser("~")
 
 sticky = []
+
 
 @hook.subscribe.setgroup
 def move_sticky():
     for window in sticky:
         window.togroup()
 
+
 @hook.subscribe.client_killed
 def remove_sticky(window):
     if window in sticky:
         sticky.remove(window)
 
+
 @hook.subscribe.client_new
 def add_sticky(window):
-    if (window.name == 'Picture-in-Picture'):
+    if window.name == "Picture-in-Picture":
         sticky.append(window)
+
 
 @lazy.function
 def toggle_sticky(qtile):
@@ -30,102 +34,97 @@ def toggle_sticky(qtile):
         sticky.remove(window)
     else:
         sticky.append(window)
-    
+
 
 groups = [
     Group("1", spawn=["codium"]),
     Group("2", spawn=["firefox"]),
-    Group("3", spawn=["obsidian"]),
+    Group("3", spawn=["alacitty"]),
     Group("4", spawn=["telegram-desktop"]),
-    Group("5", spawn=["blueman-manager", "protonvpn-app"]),
-    Group("6", spawn=["postman"]),
-] + [Group(i) for i in "789"]
+    Group("5", spawn=["blueman-manager"]),
+] + [Group(i) for i in "6789"]
 
 mod = "mod4"
-keys = [
-    Key([mod], "r", lazy.spawn("rofi -show drun")),
-    Key([mod], "e", lazy.spawn("Thunar")),
-    Key([mod], "Return", lazy.spawn("alacritty")),
-
-    Key([mod], "q", lazy.window.kill()),
-
-    Key([mod], "s", toggle_sticky),
-
-    Key([mod, "shift"], "r", lazy.reload_config()),
-
-    Key([mod], "f", lazy.window.toggle_floating()),
-    Key([mod, "shift"], "f", lazy.window.toggle_fullscreen()),
-
-    Key([mod], "v", lazy.spawn("clipmenu")),
-
-    Key([mod], "p", lazy.spawn("xcolor -S 12 -s clipboard")),
-
-    Key([ ], "Print", lazy.spawn(f"scrot {home}/MyMedia/%Y-%m-%d_%H-%M-%S.png")),
-    Key([mod], "Print", lazy.spawn(f"scrot {home}/MyMedia/%Y-%m-%d_%H-%M-%S.png -s")),
-
-    Key([mod], "F9", lazy.spawn("brightnessctl set 10%-")),
-    Key([mod], "F10", lazy.spawn("brightnessctl set +10%")),
-
-    Key([mod], "F11", lazy.spawn("pamixer -d 10")),
-    Key([mod], "F12", lazy.spawn("pamixer -i 10")),
-
-    Key([mod], "Left", lazy.layout.left()),
-    Key([mod], "Right", lazy.layout.right()),
-    Key([mod], "Up", lazy.layout.down()),
-    Key([mod], "Down", lazy.layout.up()),
-
-    Key([mod, "control"], "Left", lazy.layout.shuffle_left()),
-    Key([mod, "control"], "Right", lazy.layout.shuffle_right()),
-    Key([mod, "control"], "Up", lazy.layout.shuffle_down()),
-    Key([mod, "control"], "Down", lazy.layout.shuffle_up()),
-
-    Key([mod, "shift"], "Left", lazy.layout.grow_left()),
-    Key([mod, "shift"], "Right", lazy.layout.grow_right()),
-    Key([mod, "shift"], "Up", lazy.layout.grow_down()),
-    Key([mod, "shift"], "Down", lazy.layout.grow_up()),
-] + [
-    Key([mod], i.name, lazy.group[i.name].toscreen()) for i in groups
-] + [
-    Key([mod, "shift"], i.name, lazy.window.togroup(i.name)) for i in groups
-]
+keys = (
+    [
+        Key([mod], "r", lazy.spawn("rofi -show drun")),
+        Key([mod], "e", lazy.spawn("Thunar")),
+        Key([mod], "Return", lazy.spawn("alacritty")),
+        Key([mod], "q", lazy.window.kill()),
+        Key([mod], "s", toggle_sticky),
+        Key([mod, "shift"], "r", lazy.reload_config()),
+        Key([mod], "f", lazy.window.toggle_floating()),
+        Key([mod, "shift"], "f", lazy.window.toggle_fullscreen()),
+        Key([mod], "v", lazy.spawn("clipmenu")),
+        Key([mod], "p", lazy.spawn("xcolor -S 12 -s clipboard")),
+        Key([], "Print", lazy.spawn(f"scrot {home}/MyMedia/%Y-%m-%d_%H-%M-%S.png")),
+        Key(
+            [mod], "Print", lazy.spawn(f"scrot {home}/MyMedia/%Y-%m-%d_%H-%M-%S.png -s")
+        ),
+        Key([mod], "F9", lazy.spawn("brightnessctl set 10%-")),
+        Key([mod], "F10", lazy.spawn("brightnessctl set +10%")),
+        Key([mod], "F11", lazy.spawn("pamixer -d 10")),
+        Key([mod], "F12", lazy.spawn("pamixer -i 10")),
+        Key([mod], "Left", lazy.layout.left()),
+        Key([mod], "Right", lazy.layout.right()),
+        Key([mod], "Up", lazy.layout.down()),
+        Key([mod], "Down", lazy.layout.up()),
+        Key([mod, "control"], "Left", lazy.layout.shuffle_left()),
+        Key([mod, "control"], "Right", lazy.layout.shuffle_right()),
+        Key([mod, "control"], "Up", lazy.layout.shuffle_down()),
+        Key([mod, "control"], "Down", lazy.layout.shuffle_up()),
+        Key([mod, "shift"], "Left", lazy.layout.grow_left()),
+        Key([mod, "shift"], "Right", lazy.layout.grow_right()),
+        Key([mod, "shift"], "Up", lazy.layout.grow_down()),
+        Key([mod, "shift"], "Down", lazy.layout.grow_up()),
+    ]
+    + [Key([mod], i.name, lazy.group[i.name].toscreen()) for i in groups]
+    + [Key([mod, "shift"], i.name, lazy.window.togroup(i.name)) for i in groups]
+)
 
 screens = [
     Screen(
-        top=bar.Bar([
-            widget.GroupBox(
-                highlight_method="line",
-                highlight_color=["#000000", "#393939"],
-                inactive="#767676",
-                this_current_screen_border="#767676",
-                this_screen_border="#767676"
-            ),
-            widget.Spacer(length=4),
-            widget.WindowName(),
-
-            widget.Clock(format="%B-%d %a"),
-            widget.Spacer(length=4),
-
-            widget.Clock(format="%H:%M.%S"),
-            widget.Spacer(length=4),
-
-            widget.Net(format="↓{down:.0f}{down_suffix}"),
-            widget.Net(format="↑{up:.0f}{up_suffix}"),
-            widget.CPU(format="{load_percent:.0f}%"),
-            widget.Memory(format="{MemUsed:.1f}{mm}", measure_mem='G'),
-            widget.Spacer(length=4),
-
-            widget.Systray(),
-            widget.Spacer(length=4),
-        ], 22),
-        background="#191919"
+        top=bar.Bar(
+            [
+                widget.GroupBox(
+                    highlight_method="line",
+                    highlight_color=["#000000", "#393939"],
+                    inactive="#767676",
+                    this_current_screen_border="#767676",
+                    this_screen_border="#767676",
+                ),
+                widget.Spacer(length=4),
+                widget.WindowName(),
+                widget.Clock(format="%B-%d %a"),
+                widget.Spacer(length=4),
+                widget.Clock(format="%H:%M.%S"),
+                widget.Spacer(length=4),
+                widget.Net(format="↓{down:.0f}{down_suffix}"),
+                widget.Net(format="↑{up:.0f}{up_suffix}"),
+                widget.CPU(format="{load_percent:.0f}%"),
+                widget.Memory(format="{MemUsed:.1f}{mm}", measure_mem="G"),
+                widget.Spacer(length=4),
+                widget.Systray(),
+                widget.Spacer(length=4),
+            ],
+            22,
+        ),
+        background="#191919",
     )
 ]
 
-layouts = [ layout.Bsp() ]
+layouts = [layout.Bsp()]
 
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+    ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
