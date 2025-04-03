4c1ccd2e0947816bbeaa90c4c5682391bfc543e6
Toffee35 <nailzagru@gmail.com>
2025-04-03 15:32:32 +0300

    rust

--- system/langs.nix
+++ system/langs.nix
@@ -11,8 +11,25 @@
     rustup
     cargo-watch
 
+    pkg-config
+    openssl
+    openssl.dev
+
+    (rust-bin.stable.latest.default.override {
+      extensions = [ "rust-src" "rustfmt" ];
+    })
+
+    cmake
+    perl
+
     gcc
 
     go
   ];
+
+  environment.variables = {
+    OPENSSL_DIR = "${pkgs.openssl.dev}";
+    OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+  };
 }
