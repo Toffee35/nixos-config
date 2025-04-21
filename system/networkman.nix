b779375a81a35a227daee649b21d167cbd2eb4e9
Toffee35 <nailzagru@gmail.com>
2025-04-21 20:24:05 +0300

    wireguard

--- system/networkman.nix
+++ system/networkman.nix
@@ -1,4 +1,6 @@
-{ username, ... }: {
+{ username, pkgs, ... }: {
+  environment.systemPackages = with pkgs; [ wireguard-tools ];
+
   networking.networkmanager.enable = true;
 
   users.extraGroups.networkmanager.members = [ "${username}" ];
