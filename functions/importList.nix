a1be1d86fa71f9990211510560ed9ff763e38ea0
Toffee35 <nailzagru@gmail.com>
2025-04-28 22:21:04 +0300

    fix

--- functions/importList.nix
+++ functions/importList.nix
@@ -2,6 +2,10 @@
 let
   items = builtins.readDir dir;
   names = builtins.attrNames items;
+
   filter = name: pkgs.lib.strings.hasSuffix ".nix" name;
   filtered = builtins.filter filter names;
+
+  addPath = name: "${dir}/${name}";
+  list = builtins.map addPath filtered;
 in list
