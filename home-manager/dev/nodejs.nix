3ec6066ffce7d432373181b5afdab23361cc6cc6
Toffee35 <nailzagru@gmail.com>
2025-06-11 18:35:16 +0300

    fix

--- home-manager/dev/nodejs.nix
+++ home-manager/dev/nodejs.nix
@@ -1,4 +1,8 @@
-{pkgs, username, ...}: {
+{
+  pkgs,
+  username,
+  ...
+}: {
   home = {
     packages = with pkgs; [
       nodejs
@@ -10,11 +14,10 @@
   };
 
   programs.vscode.profiles.${username}.extensions = with pkgs.vscode-extensions; [
-      ms-python.python
-      ms-python.debugpy
-      ms-python.vscode-pylance
-      fill-labs.dependi
-      charliermarsh.ruff
-      ms-toolsai.jupyter
-    ];
+    ms-python.python
+    ms-python.debugpy
+    ms-python.vscode-pylance
+    charliermarsh.ruff
+    ms-toolsai.jupyter
+  ];
 }
