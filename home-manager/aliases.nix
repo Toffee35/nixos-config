e9b9d82e9bee7abd006eefa7bd5cd597c9454a27
Toffee35 <nailzagru@gmail.com>
2025-06-14 20:10:23 +0300

    lock update and fl-reb fix

--- home-manager/aliases.nix
+++ home-manager/aliases.nix
@@ -9,8 +9,8 @@
 
     fl-chk = "nix flake check";
     fl-up = "nix flake update --flake ${flakeDir}";
-    fl-reb = "cle && fl-up && fl-chk && os-reb && hm-reb";
-    fl-reb-f = "cle && fl-up && fl-chk && os-reb && hm-reb";
+    fl-reb = "cle && fl-up && os-reb && hm-reb";
+    fl-reb-f = "cle && fl-up && os-reb && hm-reb";
 
     nx-coll = "sudo nix-collect-garbage -d";
     nx-opt = "sudo nix store optimise";
