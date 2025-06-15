bbce50e9f15fd984da02e8f3d78d95ccf5901c65
Toffee35 <nailzagru@gmail.com>
2025-06-15 15:52:44 +0300

    fix

--- nixos/portainer.nix
+++ nixos/portainer.nix
@@ -5,14 +5,19 @@
     containers.portainer = {
       image = "portainer/portainer-ce:alpine";
 
-      ports = ["9443:9443" "8000:8000"];
+      ports = [
+        "9443:9443"
+        "8000:8000"
+      ];
 
       volumes = [
         "/run/urs/1000/docker.sock:/var/run/docker.sock"
         "portainer_data:/data"
       ];
 
-      restart = "always";
+      extraOptions = [
+        "--restart=always"
+      ];
     };
   };
 }
