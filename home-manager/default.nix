c3fcbfe023c6529eb5b3c8373260f2a2c338e798
Toffee35 <nailzagru@gmail.com>
2025-06-20 18:07:54 +0300

    fix

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -17,7 +17,7 @@
   };
 
   home.activation.createNotesDir = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    mkdir -p /mnt/Files/Notes
+    mkdir -p /mnt/Files/Notes/content
     ln -sfn /mnt/Files/Notes/content ${homedir}/Notes
   '';
 }
