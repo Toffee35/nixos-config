da6ace0d70a6bea84ce645288dcb4f22706c524e
Toffee35 <nailzagru@gmail.com>
2025-06-21 07:42:43 +0300

    git init

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -6,7 +6,10 @@
   ];
 
   programs = {
-    git.enable = true;
+    git = {
+      enable = true;
+      extraConfig.init.defaultBranch = "main";
+    };
     tmux.enable = true;
     btop.enable = true;
     lazydocker.enable = true;
