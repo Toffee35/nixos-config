d2409577a1f30c9233d6ee11b03d8400582cafeb
Toffee35 <nailzagru@gmail.com>
2025-09-03 22:34:28 +0300

    mime firefox

--- home-manager/mime.nix
+++ home-manager/mime.nix
@@ -15,6 +15,17 @@
       "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
       "application/x-zerosize" = "codium.desktop";
       "application/octet-stream" = "codium.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
+      "application/sql" = "codium.desktop";
     };
   };
 }
