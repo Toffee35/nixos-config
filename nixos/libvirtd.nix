5e6f028dafb0df781004053544840a069106b026
Toffee35 <nailzagru@gmail.com>
2025-09-18 22:40:42 +0300

    networking for vms removed

--- nixos/libvirtd.nix
+++ nixos/libvirtd.nix
@@ -35,32 +35,4 @@
     qemuGuest.enable = true;
     spice-vdagentd.enable = true;
   };
-
-  networking = {
-    useDHCP = false;
-    useNetworkd = true;
-  };
-
-  systemd.network = {
-    enable = true;
-
-    netdevs."br0" = {
-      Kind = "bridge";
-      Name = "br0";
-    };
-
-    networks = {
-      "10-br0-dhcp" = {
-        Name = "br0";
-        DHCP = "ipv4";
-      };
-
-      "20-ethernet-br0" = {
-        Name = "enp2s0f1";
-        Network = "br0";
-      };
-    };
-  };
-
-  networking.firewall.checkReversePath = "loose";
 }
