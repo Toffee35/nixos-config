b0db8cf8dffc28db487ac68f9f266295e8e8160e
Toffee35 <nailzagru@gmail.com>
2025-09-22 21:35:20 +0300

    added vscode plugin golang

--- home-manager/dev/golang.nix
+++ home-manager/dev/golang.nix
@@ -1,7 +1,15 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  username,
+  ...
+}: {
   home.packages = with pkgs; [
     go
 
     jetbrains.goland
+  ];
+
+  programs.vscode.profiles.${username}.extensions = with pkgs.vscode-extensions; [
+    golang.go
   ];
 }
