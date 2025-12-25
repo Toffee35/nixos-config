b7489d9a3c54012dc5d3477cad0f3321b644b17f
Toffee35 <nailzagru@gmail.com>
2025-12-25 22:52:02 +0300

    fix

--- home-manager/dev/terraform.nix
+++ home-manager/dev/terraform.nix
@@ -4,16 +4,16 @@
   ...
 }: {
   home.packages = with pkgs; [
-    terraform.withPlugins
-    (p: [
-      p.libvirt
-      p.docker
-      p.kubernetes
-      p.helm
-      p.local
-      p.tls
-      p.random
-    ])
+    (terraform.withPlugins
+      (p: [
+        p.libvirt
+        p.docker
+        p.kubernetes
+        p.helm
+        p.local
+        p.tls
+        p.random
+      ]))
   ];
 
   programs.vscode.profiles.${username}.extensions = with pkgs.vscode-extensions; [
